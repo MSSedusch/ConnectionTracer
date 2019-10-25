2c6b82bb0db9837e8e0ae53b237b7a1bda72dc70
MSSedusch <sedusch@microsoft.com>
2019-10-25 15:46:53 +0200

    Add .NET Core Client application for CF

---
+++ ClientCore/Client.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClientCore
+{
+    class Client
+    {
+        [Flags]
+        public enum ConnectionState
+        {
+            Initial = 0,
+            Connected = 1,
+            Disconnected = 2,
+            Read = 4,
+            Write = 8
+        }
+
+        public long ErrorOnConnect { get; set; }
+        public long ErrorOnRead { get; set; }
+        public long ErrorOnWrite { get; set; }
+        public long MessageRead { get; set; }
+        public long MessageSent { get; set; }
+        public string OutboundIP { get; set; }
+        public string Server { get; set; }
+        public ConnectionState State { get; set; }
+
+        public Client()
+        {
+            this.State = ConnectionState.Initial;
+        }
+
+
+        public async void StartClientAsync(string remoteHost, int remotePort, int sleepTime)
+        {
+            TcpClient client;
+
+            try
+            {
+                client = new TcpClient();
+                await client.ConnectAsync(remoteHost, remotePort);
+            }
+            catch (Exception ex)
+            {
+                client = null;
+                ErrorOnConnect++;
+
+                Program.Log($"Error on Connected: {ex.Message}", true);
+            }
+
+            if (client == null)
+            {
+                State = ConnectionState.Disconnected;
+                return;
+            }
+
+            State = ConnectionState.Connected;
+
+            var stream = client.GetStream();
+            StreamWriter writer = new StreamWriter(stream);
+            StreamReader reader = new StreamReader(stream);
+
+            while (true)
+            {
+                try
+                {
+                    var message = await reader.ReadLineAsync();
+                    Program.Log($"Read {message}", true);
+                    var matches = Regex.Matches(message, @".* (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d*).* (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d*).*\[(.*)\]");
+                    if (matches.Count == 1)
+                    {
+                        if (matches[0].Groups.Count == 6)
+                        {
+                            var outIp = matches[0].Groups[1].Value;
+                            var server = matches[0].Groups[5].Value;
+                            this.OutboundIP = outIp;
+                            this.Server = server;
+                        }
+                        else
+                        {
+                            Program.Log("error extracting out IP");
+                        }
+                    }
+                    else
+                    {
+                        Program.Log("error extracting out IP");
+                    }
+                    State |= ConnectionState.Read;
+                    MessageRead++;
+                }
+                catch (Exception ex)
+                {
+                    ErrorOnRead++;
+                    Program.Log($"Error on Read: {ex.Message}", true);
+                    break;
+                }
+
+                await Task.Delay(sleepTime);
+
+                try
+                {
+                    await writer.WriteLineAsync("Next");
+                    await writer.FlushAsync();
+                    Program.Log($"Write Next", true);
+                    State |= ConnectionState.Write;
+                    MessageSent++;
+                }
+                catch (Exception ex)
+                {
+                    ErrorOnWrite++;
+                    Program.Log($"Error on Write: {ex.Message}", true);
+                    break;
+                }
+            }
+
+            State = ConnectionState.Disconnected;
+        }
+    }
+}
---
+++ ClientCore/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientCore
+{ 
+    class Program
+    {
+        class ConnectionState
+        {
+            public State State { get; set; }
+            public String OutboundIP { get; set; }
+            public string Server { get; set; }
+        }
+
+        [Flags]
+        enum State
+        {
+            Initial,
+            Connected,
+            Disconnected,
+            Read,
+            Write
+        }
+
+        static List<Client> _Clients = new List<Client>();
+        static bool verbose = false;
+
+        static void Main(string[] args)
+        {
+            Log("#################### Starting yeah ##############################");
+            try
+            {
+                RunAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+
+                Console.Write(ex.ToString());
+                throw;
+            }
+        }
+
+        private static async Task RunAsync()
+        {
+            int connections = 50000;
+            string remoteHost = "server-lb.westeurope.cloudapp.azure.com";
+            int remotePort = 9005;
+            int runtime = -1;
+            int maxConnected = 0;
+            int sleepTime = 60000;
+            DateTime startTime = DateTime.Now;
+
+            //int connections = 10;
+            //string remoteHost = "localhost";
+            //int remotePort = 9005;            
+
+            while (true)
+            {
+                if ((runtime > 0) && ((DateTime.Now - startTime).TotalSeconds > runtime))
+                {
+                    Log($"runtime reached. Stopping. Test result: max connected={maxConnected}");
+                    break;
+                }
+
+                int running = _Clients.Count(cl => (cl.State & Client.ConnectionState.Disconnected) == Client.ConnectionState.Initial);
+                int connected = _Clients.Count(cl => (cl.State & Client.ConnectionState.Connected) == Client.ConnectionState.Connected);
+
+                for (int i = running; i < connections; i++)
+                {
+                    Client newClient = new Client();
+                    _Clients.Add(newClient);
+                    newClient.StartClientAsync(remoteHost, remotePort, sleepTime);
+                }
+                int newThreads = connections - running;
+                var ipArray = _Clients.Select(val => val.OutboundIP).Distinct().ToArray();
+                string ips = "[]";
+                if (ipArray.Length > 0)
+                {
+                    ips = String.Join(",", ipArray);
+                }
+                var serverArray = _Clients.Select(val => val.Server).Distinct().ToArray();
+                string servers = "[]";
+                if (serverArray.Length > 0)
+                {
+                    servers = String.Join(",", serverArray);
+                }
+
+                long errorOnConnect = _Clients.Sum(cl => cl.ErrorOnConnect);
+                long errorOnWrite = _Clients.Sum(cl => cl.ErrorOnWrite);
+                long errorOnRead = _Clients.Sum(cl => cl.ErrorOnRead);
+                long messageSent = _Clients.Sum(cl => cl.MessageSent);
+                long messageRead = _Clients.Sum(cl => cl.MessageRead);
+
+                Log($"Status: Threads: running={running} connected={connected} total={_Clients.Count} Target:{connections} new:{newThreads} outIps:{ips} servers:{servers} msg sent={messageSent} msg read={messageRead} errorOnConnect={errorOnConnect} errorOnWrite={errorOnWrite} errorOnRead={errorOnRead}");
+                maxConnected = Math.Max(maxConnected, connected);
+
+                await Task.Delay(10000);
+            }
+        }
+
+        internal static void Log(string message, bool msgVerbose = false)
+        {
+            if ((!msgVerbose) || (msgVerbose && verbose))
+            {
+                Console.WriteLine($"[{DateTime.Now.ToString("yyyyMMdd hh:mm:ss")}] {message}");
+            }
+        }
+    }
+}
